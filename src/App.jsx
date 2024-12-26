c3943f5748b68cd37f41a7df11c2c943c4610e1d
masum362 <masumahmed64077@gmail.com>
2024-12-26 10:13:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Router from './router/router'
+
+const App = () => {
+  return (
+    <Router />
+  )
+}
+
+export default App
---
+++ src/router/Router.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from '../pages/home/Home'
+import Login from '../pages/login/Login'
+import Register from '../pages/register/Register'
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
