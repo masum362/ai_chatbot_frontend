230b04c2cea41654c532a8850d16bf6c173c5333
masum362 <masumahmed64077@gmail.com>
2024-12-28 15:29:42 +0600

    home page functionality added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -26,10 +26,22 @@ const Home = () => {
     }
   }
 
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const { data } = await axios.get('/project/all')
+        setProjects(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProjects()
+  }, [])
+
 
   return (
     <main className='p-4'>
-      <div className="projects flex flex-wrap gap-3">
+      <div className="projects flex flex-col items-start flex-wrap gap-3">
         <button
           onClick={() => setIsModalOpen(true)}
           className="project p-4 border border-slate-300 rounded-md">
@@ -37,7 +49,27 @@ const Home = () => {
           <i className="ri-link ml-2"></i>
         </button>
 
+        {
+          projects.map((project) => (
+            <div key={project._id}
+              onClick={() => {
+                navigate(`/project`, {
+                  state: { project }
+                })
+              }}
+              className="project flex flex-col gap-2 cursor-pointer p-4 border border-slate-300 rounded-md min-w-52 hover:bg-slate-200">
+              <h2
+                className='font-semibold'
+              >{project.name}</h2>
 
+              <div className="flex gap-2">
+                <p> <small> <i className="ri-user-line"></i> Collaborators</small> :</p>
+                {project.users.length}
+              </div>
+
+            </div>
+          ))
+        }
       </div>
 
       {isModalOpen && (
